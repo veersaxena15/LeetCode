d0db8dda58710571502fb4b67e0ade8be42c5cb8
veersaxena15 <veersaxena973@gmail.com>
2026-06-19 11:42:19 +0530

    Time: 1 ms (21.24%), Space: 43.3 MB (75.71%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -3,13 +3,13 @@ class Solution {
         int n = nums.length;
          for(int i = 0; i < n - 1; i++) 
          {
-            for(int j = 0; j < n - i - 1; j++) 
+            for(int j = i+1; j < n; j++) 
             {
-                if(nums[j] > nums[j + 1]) 
+                if(nums[i] > nums[j]) 
                 {
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
                 }
             }
         }
