402d2595ebf9efc032d8536f2399c9a60359b0a4
veersaxena15 <veersaxena973@gmail.com>
2026-06-21 10:09:28 +0530

    Time: 1 ms (98.14%), Space: 42.5 MB (79.61%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -17,6 +17,12 @@ class Solution {
         //}
         //return sum;
         if(num == 0) return 0;
-        return 1 + (num - 1) % 9;
+        // return 1 + (num - 1) % 9;
+        if(num % 9 == 0)
+          return 9;
+
+        return num % 9;
+
+       
     }
 }
