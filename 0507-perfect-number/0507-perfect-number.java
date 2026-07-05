7f5b854bf882b93032473be3cfbe90d16bbc1147
veersaxena15 <veersaxena973@gmail.com>
2026-07-05 23:46:20 +0530

    Time: 2 ms (83.81%), Space: 42.3 MB (26.01%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
        
-        if(num<=0){
+        if(num<=1){
             return false;
         }
-        int sum = 0;
-        for(int i=1;i*i<=num;i++){
+        int sum = 1;
+        for(int i=2;i*i<=num;i++){
             if(num%i==0){
                 sum+= i;
                 if(i * i != num){
@@ -13,6 +13,6 @@ class Solution {
                 }
             }
         }
-        return sum - num == num;
+        return sum == num;
     }
 }
