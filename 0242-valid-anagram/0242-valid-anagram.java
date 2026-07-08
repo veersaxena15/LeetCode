5d44cb46e3e07d28827563f918feed6c31282cba
Veer Saxena <veersaxena973@gmail.com>
2026-07-09 00:42:02 +0530

    Time: 6 ms (43.77%), Space: 44.3 MB (82.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,34 +1,50 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        String str3 = "", str4 = "";
-        if(s.length() == t.length())
-        {
-        for(int i = 65; i <= 90; i++)
-        {
-            String str1 = s.toUpperCase();
-            String str2 = t.toUpperCase();
-
-            for(int j = 0; j < t.length(); j++)
-            {
-                char ch1 = str1.charAt(j);
-                char ch2 = str2.charAt(j);
-
-                int as1 = (int)ch1;
-                int as2 = (int)ch2;
-
-                if(as1 == i)
-                {
-                    str3 = str3 + ch1;
-                }
-                if(as2 == i)
-                {
-                    str4 = str4 + ch2;
-                }
-            }
-        }
-        return str3.equals(str4);
-        }
+        if(s.length() != t.length())
         return false;
+
+        int[] count = new int[26];
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for(int i = 0; i < 26; i++)
+        {
+            if(count[i] != 0)
+            return false;
+        }
+        
+        return true;
+
+        // String str3 = "", str4 = "";
+        // if(s.length() == t.length())
+        // {
+        // for(int i = 97; i <= 122; i++)
+        // {
+        //     for(int j = 0; j < t.length(); j++)
+        //     {
+        //         char ch1 = s.charAt(j);
+        //         char ch2 = t.charAt(j);
+
+        //         int as1 = (int)ch1;
+        //         int as2 = (int)ch2;
+
+        //         if(as1 == i)
+        //         {
+        //             str3 = str3 + ch1;
+        //         }
+        //         if(as2 == i)
+        //         {
+        //             str4 = str4 + ch2;
+        //         }
+        //     }
+        // }
+        // return str3.equals(str4);
+        // }
+        // return false;
     }
 }
