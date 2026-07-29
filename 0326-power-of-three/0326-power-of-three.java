609c6133c6e034b6ae1dd4aa006a8ef07bbe8b1e
Veer Saxena <veersaxena973@gmail.com>
2026-07-30 00:30:53 +0530

    Time: 8 ms (92.59%), Space: 46.3 MB (10.58%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,12 +1,18 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
 
-        if(n  <= 0)
-            return false;
+        //If n is a power of 3, then it must divide 1162261467
+        //If n is not a power of 3 (e.g., 6, 12, 45), it cannot divide 3^19 exactly
+        return n > 0 && 1162261467 % n == 0;
+        
 
-        while(n % 3 == 0)
-            n = n / 3;
 
-        return n == 1;
+        // if(n  <= 0)
+        //     return false;
+
+        // while(n % 3 == 0)
+        //     n = n / 3;
+
+        // return n == 1;
     }
 }
