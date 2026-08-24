a749336940c5b591221749994d7095ec3c176fe0
Veer Saxena <veersaxena973@gmail.com>
2026-08-25 01:12:50 +0530

    Time: 0 ms (100%), Space: 42.9 MB (38.07%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+        
+        int prev1 = 0;
+        int prev2 = 0;
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            int current = Math.max(prev1, prev2 + nums[i]);
+
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
+}
